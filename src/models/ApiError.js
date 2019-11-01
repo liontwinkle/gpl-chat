128804037c8b687cf36bb0f7ce4f45b0075ccdbf
liontwinkle <lion.twinkle.ge@gmail.com>
2019-11-01 20:38:57 +0200

    Better error handing

---
+++ src/models/ApiError.js
@@ -0,0 +1,22 @@
+import { get } from 'lodash';
+
+export class ApiError {
+  constructor({
+    message,
+    status,
+    originalError
+  }) {
+    this.message = message;
+    this.status = status;
+    this.originalError = originalError;
+  }
+
+  static from(originalError) {
+    const parsedErr = get(originalError, 'graphQLErrors[0]', originalError);
+    return new ApiError({
+      message: get(parsedErr, 'message', 'Something went wrong'),
+      status: get(parsedErr, 'status', 500),
+      originalError,
+    });
+  };
+}
--- src/store/auth/saga.js
+++ src/store/auth/saga.js
@@ -3,6 +3,7 @@ import { authActionNames } from '.';
 import { FORM_ERROR } from 'final-form';
 import api from '../../api';
 import { authActions } from './actions';
+import { ApiError } from '../../models/ApiError';
 
 function* registerUser({ payload: { email, password, name } }) {
   try {
@@ -22,8 +23,12 @@ function* registerUser({ payload: { email, password, name } }) {
 
     yield put(authActions.userLoggedIn({ user, token }));
     yield put(authActions.registerUserSuccess());
-  } catch (error) {
-    yield put(authActions.registerUserError({ [FORM_ERROR]: 'Got an error :/' }));
+  } catch (err) {
+    yield put(
+      authActions.registerUserError({
+        [FORM_ERROR]: ApiError.from(err).message,
+      })
+    );
   }
 }
 
